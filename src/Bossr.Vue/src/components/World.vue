a8a009b6c769e32b5e92c475dabfa8981ed4bca8
Niklas Näslund <naaaslund@gmail.com>
2017-06-07 02:35:44 +0200

    Added some styling Added a progressbar for each state

--- src/Bossr.Vue/src/components/World.vue
+++ src/Bossr.Vue/src/components/World.vue
@@ -3,19 +3,19 @@
     <div class="columns is-multiline">
       <div class="column is-one-third" v-for="state in states">
         <div class="box">
-          <p>{{ state.raid.frequencyHoursMin }} - {{ state.raid.frequencyHoursMax }} hours</p>
+          <span class="is-pulled-right"><b-icon icon="call_missed" />{{ state.missedRaids }}</span>
           <div v-for="spawn in state.raid.spawns" :key="spawn.id">
-            <p>{{ spawn.creature.name }}</p>
+            <p><strong>{{ spawn.creature.name }}</strong></p>
             <p v-for="position in spawn.positions" :key="position.id">
               {{ position.name }}
-              <b-icon icon="place"></b-icon>
+              <b-icon icon="place" />
             </p>
+            <hr>
           </div>
-          <hr>
-          <p>Occurs between:</p>
-          <p>{{ getFormattedTime(state.expectedMin) }} - {{ getFormattedTime(state.expectedMax) }}</p>
-          <hr>
-          <p>Missed: {{ state.missedRaids }}</p>
+          <div>
+            <p style="text-align: center; margin: 1.5rem"><b-icon icon="access_time" /> {{ getFormattedTime(state.expectedMin) }} - {{ getFormattedTime(state.expectedMax) }}</p>
+            <progress class="progress" v-bind:value="getProgress(state.expectedMin, state.expectedMax)" max="100" />
+          </div>
         </div>
       </div>
     </div>
@@ -33,30 +33,33 @@ export default {
   },
   created () {
     this.$http.get('http://localhost:5000/api/states/' + this.$route.params.id).then(response => {
-      console.log(response.body)
-      this.states = response.body
+      function compare (a, b) {
+        if (a.raid.spawns[0].creature.name < b.raid.spawns[0].creature.name) {
+          return -1
+        }
+        if (a.raid.spawns[0].creature.name > b.raid.spawns[0].creature.name) {
+          return 1
+        }
+        return 0
+      }
+      this.states = response.body.sort(compare)
     })
   },
   methods: {
+    getProgress (min, max) {
+      let minUnix = moment(min).unix()
+      let maxUnix = moment(max).unix()
+
+      let now = moment().unix()
+
+      let maxValue = maxUnix - minUnix
+      let currentValue = now - minUnix
+
+      return Math.round((currentValue / maxValue) * 100)
+    },
     getFormattedTime (time) {
       return moment(time).fromNow()
     }
   }
 }
 </script>
-
-<style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-</style>
